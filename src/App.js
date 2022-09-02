6e031aa201032fe675db3cd84076e93a75940512
Proloycb <proloychacroborty@gmail.com>
2022-09-02 16:16:56 +0600

    add some route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import SignUp from './pages/Login/SignUp';
+import Footer from './pages/Shared/Footer';
+import Header from './pages/Shared/Header';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="max-w-7xl mx-auto">
+      <Header/>
+      <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/signup' element={<SignUp/>}/>
+      </Routes>
+      <Footer/>
     </div>
   );
 }
